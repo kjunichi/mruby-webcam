aefd025ecce7a809f49e7e10c41660f247628bcf
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-09-02 11:42:39 +0900

    Fix test

--- test/mrb_webcam.rb
+++ test/mrb_webcam.rb
@@ -2,16 +2,6 @@
 ## Webcam Test
 ##
 
-assert("Webcam#hello") do
-  t = Webcam.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("Webcam#bye") do
-  t = Webcam.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
 assert("Webcam.hi") do
   assert_equal("hi!!", Webcam.hi)
 end
